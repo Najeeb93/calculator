cedd4f2133eb6f73744d0ab323072455146c951d
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-05-17 20:47:23 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+var result = document.getElementById("result");
+
+function getNumber(num){
+    result.value += num;
+}
+
+function clearResult(){
+    result.value = "";
+}
+
+function getResult(){
+    result.value = eval(result.value);
+
+    
+
+}
+
+function deletNum(){
+    
+    result.value = result.value.slice(0,-1)
+}
